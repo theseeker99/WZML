14406c07eef6123a712c154962107d37e9e2d21a
Silent Demon <105407900+SilentDemonSD@users.noreply.github.com>
2022-11-21 10:29:53 +0530

    Sorry 😞

--- bot/helper/mirror_utils/download_utils/qbit_downloader.py
+++ bot/helper/mirror_utils/download_utils/qbit_downloader.py
@@ -196,23 +196,23 @@ def __check_limits(client, tor):
                     msg += f'\n#Buy Paid Service'
                 __onDownloadError(msg, client, tor)
                 return
-                limit = None
-                if ZIP_UNZIP_LIMIT is not None and arch:
-                    mssg = f'Zip/Unzip limit is {ZIP_UNZIP_LIMIT}GB'
-                    limit = ZIP_UNZIP_LIMIT
-                if LEECH_LIMIT is not None and listener.isLeech:
-                    mssg = f'Leech limit is {LEECH_LIMIT}GB'
-                    limit = LEECH_LIMIT
-                elif TORRENT_DIRECT_LIMIT is not None:
-                    mssg = f'Torrent limit is {TORRENT_DIRECT_LIMIT}GB'
-                    limit = TORRENT_DIRECT_LIMIT
-                if PAID_SERVICE is True:
-                    mssg += f'\n#Buy Paid Service'
-                if limit is not None:
-                    LOGGER.info('Checking File/Folder Size...')
-                    if size > limit * 1024**3:
-                        fmsg = f"{mssg}.\nYour File/Folder size is {get_readable_file_size(size)}"
-                        __onDownloadError(fmsg, client, tor)
+        limit = None
+        if ZIP_UNZIP_LIMIT is not None and arch:
+            mssg = f'Zip/Unzip limit is {ZIP_UNZIP_LIMIT}GB'
+            limit = ZIP_UNZIP_LIMIT
+        if LEECH_LIMIT is not None and listener.isLeech:
+            mssg = f'Leech limit is {LEECH_LIMIT}GB'
+            limit = LEECH_LIMIT
+        elif TORRENT_DIRECT_LIMIT is not None:
+            mssg = f'Torrent limit is {TORRENT_DIRECT_LIMIT}GB'
+            limit = TORRENT_DIRECT_LIMIT
+        if PAID_SERVICE is True:
+            mssg += f'\n#Buy Paid Service'
+        if limit is not None:
+            LOGGER.info('Checking File/Folder Size...')
+            if size > limit * 1024**3:
+                fmsg = f"{mssg}.\nYour File/Folder size is {get_readable_file_size(size)}"
+                __onDownloadError(fmsg, client, tor)
 
 @new_thread
 def __onDownloadComplete(client, tor):
